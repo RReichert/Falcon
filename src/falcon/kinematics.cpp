edf505ff8b8ffd7e0f184b69d2d271d3e599c16b
Rodrigo Reichert <reichert.rodrigo@gmail.com>
2013-10-22 10:45:29 +1100

    identified issue with the encode torque calculate, was not accounting for the fact that the values were based on the mathematical model and not the motor values

--- src/falcon/kinematics.cpp
+++ src/falcon/kinematics.cpp
@@ -16,11 +16,27 @@ void Kinematics::decodeTheta(const boost::array<int, 3> (&encodedTheta), boost::
 }
 
 void Kinematics::encodeTorque(const boost::array<double, 3> (&omega), const boost::array<double, 3> (&torque), boost::array<int, 3> (&encodedTorque)) {
+
+  // declare function variables
+  double motorOmega, motorTorque;
+
+  // calculate the encoded torque
   for(int x=0; x<3; x++) {
-    encodedTorque[x] = torque[x] / (Ks + Kd*omega[x]);
+    motorOmega = gain*omega[x];
+    motorTorque = -torque[x]/gain;
+    encodedTorque[x] = motorTorque / (Ks + Kd*motorOmega);
+// XXX: need to check if the polarity of this encoded torque is correct
   }
 
-  // NOTE: this equation was taken directly from "Characterisation of the Novint Falcon Haptic Device for Application as a Robot Manipulator", however a possible modification would be nice to account for the change in current within the dc motors internal electric component  
+  // NOTE: derivation was based on the constraint:
+  //
+  //         arm_torque = arm_radius*F && motor_torque = motor_shaft_radius*F
+  //
+  //       because F is the common term (interaction force), we can solve for motor torque given theta
+  //       following that we use the constraint expressed in decodedTheta and convert omega to motor omega
+  //       and use these two values to convert to the motor encoded value
+
+  // NOTE:  the equation to encode motor torque was taken directly from "Characterisation of the Novint Falcon Haptic Device for Application as a Robot Manipulator", however in the future I would like to try and modify it a bit to account for the change in current within the dc motors internal electric component caused by the back emf
 }
 
 void Kinematics::d_dt(const boost::array<double, 3> (&currentValue), const boost::array<double, 3> (&prevValue), double dt, boost::array<double, 3> (&dValue_dt)) {
